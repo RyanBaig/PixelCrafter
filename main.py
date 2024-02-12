8b33d02a618d7d77132906538f804125e0c4239a
RyanBaig <rg5373429@gmail.com>
2024-02-12 15:43:22 +0500

    Now using Streamlit!

---
+++ main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import requests
+from dotenv import load_dotenv
+import os
+from io import BytesIO
+
+load_dotenv()
+
+st.title("PixelCrafter by Ryan")
+prompt = st.text_area("Prompt:", height=200)
+
+
+if st.button("Generate Pixel Art"):
+    api_url = "https://api-inference.huggingface.co/models/nerijs/pixel-art-xl"
+    token = os.getenv("PC_TOKEN")
+
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    body = f"pixel art, {prompt}"
+    response = requests.post(api_url, headers=headers, data=body)
+
+    # Check if request was successful
+    if response.status_code == 200:
+        # Convert binary data to image
+        image_data = BytesIO(response.content)
+        st.image(image_data, caption="Generated Pixel Art", use_column_width=True)
+    else:
+        st.error("Failed to generate image. Please try again.")
