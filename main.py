f1526158cfbdefac22eea43e46bf4a98f9ff417d
RyanBaig <rg5373429@gmail.com>
2024-02-12 20:27:56 +0500

    Enhance Pixel Art Generation Process
    Summary:
    
    - Wrapped the pixel art generation process within a loading spinner to provide visual feedback to users while the request is being processed.
    
    - Added informative error messages to handle cases where the request fails, providing details such as response text, status code, and a truncated token for logging purposes.
    
    Details:
    
    - Enclosed the pixel art generation logic within a with `st.spinner()` block to display a loading spinner while the request is in progress.
    
    - Updated error handling to present a formatted error message with relevant details, including response text, status code, and a truncated version of the token for logging purposes.
    
    - Minor code formatting improvements for better readability.
    
    - Optimized dependencies

--- main.py
+++ main.py
@@ -7,20 +7,28 @@ prompt = st.text_area("Prompt:", height=200)
 
 
 if st.button("Generate Pixel Art"):
-    api_url = "https://api-inference.huggingface.co/models/nerijs/pixel-art-xl"
-    token = st.secrets["TOKENS"]["PC_TOKEN"]
+    with st.spinner():
+        api_url = "https://api-inference.huggingface.co/models/nerijs/pixel-art-xl"
+        token = st.secrets["TOKENS"]["PC_TOKEN"]
 
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
+        headers = {
+            "Authorization": f"Bearer {token}"
+        }
 
-    body = f"pixel art, {prompt}"
-    response = requests.post(api_url, headers=headers, data=body)
+        body = f"pixel art, {prompt}"
+        response = requests.post(api_url, headers=headers, data=body)
 
-    # Check if request was successful
-    if response.status_code == 200:
-        # Convert binary data to image
-        image_data = BytesIO(response.content)
-        st.image(image_data, caption="Generated Pixel Art", use_column_width=True)
-    else:
-        st.error("Failed to generate image. Please try again: " + response.text + "And Status: " + str(response.status_code) + "token: " + str(token))
+        # Check if request was successful
+        if response.status_code == 200:
+            # Convert binary data to image
+            image_data = BytesIO(response.content)
+            st.image(image_data, caption="Generated Pixel Art", use_column_width=True)
+        else:
+            st.error(f"""
+            Failed to generate image. Please try again.
+            
+            LOGGING:
+            Response Text: {str(response.text)}
+            Response Status Code: {str(response.status_code)}
+            Token: {str(token[0:9])}
+            """)
